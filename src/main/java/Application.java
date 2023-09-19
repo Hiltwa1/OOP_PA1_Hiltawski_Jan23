99e9eaf1afe13a7ae5f54872f9943b4a71d9cbdb
Bartosz Hiltawski <bartek.hiltawski@o2.pl>
2023-09-19 11:30:06 +0200

    fixed no event bug

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -10,7 +10,7 @@ public class Application {
     public static void main(String[] args) {
         EventSimulation eventSimulation = new EventSimulationImpl();
         Random random = new Random();
-        for (int i = 0; i < random.nextInt(5); i++) {
+        for (int i = 0; i < random.nextInt(5) + 1; i++) {
             eventSimulation.organiseEvent(EventType.CONFERENCE, EventOrg.staff, EventOrg.guests);
             eventSimulation.organiseEvent(EventType.WEDDING, EventOrg.staff, EventOrg.guests);
             eventSimulation.organiseEvent(EventType.COCKTAIL_PARTY, EventOrg.staff, EventOrg.guests);
